5aac9f096ba57908f6d3de3843586abdf65a7653
rew326 <revian326@gmail.com>
2021-01-06 19:18:39 +0000

    working ytdl codelabs

--- ytdl2/main.py
+++ ytdl2/main.py
@@ -1,8 +1,23 @@
-import pytube
+from pytube import Playlist
+from pytube import YouTube
 
-url = 'https://www.youtube.com/watch?v=mFCKLBsmF_w'
+# Get a playlist
+playlist = Playlist('https://www.youtube.com/playlist?list=PLg1MZ2KOzNJxdrmSXv0fQ9a5vuyfWtdKv')
 
-youtube = pytube.YouTube(url)
-video = youtube.streams.first()
+# Find number of videos in the Playlist
+no_vids = len(playlist)
+print("The playlist has", no_vids, "videos")
 
-video.download('')
+# Print title of each playlist
+'''
+for url in playlist:
+    video = YouTube(url)
+    print(video.title)
+'''
+
+# Download all videos
+
+for url in playlist:
+    video = YouTube(url)
+    video.streams.first().download()
+    print("Video:",video.title,"downloaded")
