3b9f21df52d866224652d581ad7c0ec98e3964e5
rew326 <revian326@gmail.com>
2021-01-11 19:21:51 +0000

    complete v1.0

---
+++ codelabs/ex3_lists_strings.py
@@ -0,0 +1,18 @@
+# Task 1. Given a list, print the same list without the first and last element
+
+t = [1, 2, 3, 4, 5, 6]
+
+
+# Task 2. Print every other character in the given string
+
+text = "London Zoo have reportedly rejected Ricky Gervais’ wish to be fed to their lions after his death."
+
+
+# Task 3. Check if 42 is in the list of numbers:
+
+numbers = [11, 12, 19, 33, 36, 43, 64, 67, 74, 79, 101, 102, 109, 115, 116, 121, 154, 169, 174, 186, 199, 205, 223, 227, 234, 241, 250, 256, 266, 274, 276, 282, 294, 305, 307, 331, 347, 348, 369, 382, 385, 406, 410, 419, 423, 436, 456, 458, 460, 468, 485, 499, 503, 514, 526, 531, 540, 567, 573, 586, 587, 605, 607, 634, 636, 648, 661, 663, 666, 672, 685, 700, 712, 715, 721, 736, 742, 759, 767, 790, 818, 862, 863, 874, 876, 882, 887, 889, 890, 893, 894, 908, 910, 916, 917, 928, 955, 961, 972]
+
+
+# Task 4. Fill a list with all personal information given in the text (e.g.: ["Bruce","Wayne",...]):
+
+text = "First Name:Tony;Last Name:Stark;Alias:Iron Man;Publisher: Marvel Comics;First appearance:Tales of Suspense;Created by:Stan Lee, Larry Lieber, Don Heck, Jack Kirby;Place of origin: Long Island, New York;Team affiliations: Avengers"
---
+++ codelabs/solutions/ex5_functions_sol.py
@@ -0,0 +1,48 @@
+# Task 1.: Write a Python function sumNumbers to sum all the numbers in a list.
+
+def sumNumbers(numberList):
+    total = 0
+
+    for i in numberList:
+        total += i
+
+    print(total)
+
+sumNumbers([11, 12, 19, 33, 36, 43, 64, 67, 74, 79, 101, 102, 109, 115, 116, 121, 154, 169, 174, 186, 199, 205, 223, 227, 234, 241, 250, 256, 266, 274, 276, 282, 294, 305, 307, 331, 347, 348, 369, 382, 385, 406, 410, 419, 423, 436, 456, 458, 460, 468, 485, 499, 503, 514, 526, 531, 540, 567, 573, 586, 587, 605, 607, 634, 636, 648, 661, 663, 666, 672, 685, 700, 712, 715, 721, 736, 742, 759, 767, 790, 818, 862, 863, 874, 876, 882, 887, 889, 890, 893, 894, 908, 910, 916, 917, 928, 955, 961, 972])
+
+# Task 2.:  Write a Python function isPalindrome that checks whether a passed string is palindrome or not.
+# Note: A palindrome is a word, phrase, or sequence that reads the same backward as forward,
+# e.g., madam or nurses run.
+def isPalindrome(line):
+    if line == line[::-1]:
+        print(line,"is a palindrome")
+    else:
+        print(line,"is not a palindrome")
+
+isPalindrome("Red rum, sir, is murder")
+
+# Task 3.: Write a function called fizz_buzz that takes a number.
+# - If the number is divisible by 3, it should return “Fizz”.
+# - If it is divisible by 5, it should return “Buzz”.
+# - If it is divisible by both 3 and 5, it should return “FizzBuzz”.
+# - Otherwise, it should return the same number.
+
+# Task 4.: Audi Tor, an accountant at ErnstCoopers, has been asked to do an audit
+# of invoices with each having a document ID. Write a function which
+# accepts 2 parameters - a list of document IDs and number X of samples you
+# want from the list. Return a list of X random (.rand) document IDs.
+
+import random
+
+def sample(documents,x):
+    sampleList = []
+    noDocs = len(documents)
+
+    for i in range(x):
+        sampleId = random.randrange(noDocs)
+        sampleList.append(documents[sampleId])
+        noDocs -= 1
+
+    print(sampleList)
+
+sample(range(1000),278)
